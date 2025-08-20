3f6bb3d16ed90a7f8f8c270aa78896fa648af18b
WeskleyReis <weskleyff88004216@gmail.com>
2025-08-19 21:28:45 -0300

    Adiciona endpoints para ListTask e Task com autenticação e configurações de roteamento

--- backend/task/urls.py
+++ backend/task/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
+from .views import ListTaskViewSet, TaskViewSet
+from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 app_name = 'task'
+
+task_api = SimpleRouter()
+task_api.register('list-task', ListTaskViewSet, basename='list-task')
+task_api.register('task', TaskViewSet, basename='task')
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
+
+urlpatterns += task_api.urls
--- backend/task/views.py
+++ backend/task/views.py
@@ -1,5 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .serializers import ListTaskSerializer, TaskSerializer
+from .models import ListTask, Task
 
 
+class ListTaskViewSet(ModelViewSet):
+    queryset = ListTask.objects.all()
+    serializer_class = ListTaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['post', 'patch', 'delete']
+
+    def get_queryset(self):
+        return self.queryset.filter(list_task__user=self.request.user)
